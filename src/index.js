7f6fc3081e649cebaf0ed100f84a0efdf2d3d02f
EunseokGo <gomonk3037@gmail.com>
2018-07-22 18:22:23 +0900

    Create rotating square

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import Three from 'three';
+const THREE = require('three');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(10, 10, 10);
+const material = new THREE.MeshLambertMaterial({ color: 'red' });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const light = new THREE.PointLight('#fff', 1);
+light.position.set(0, 0, 100);
+
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
+camera.position.z = 100;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
+animate();
